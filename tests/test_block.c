aaeece6bf8f0b9272c8d0288156eb1f5a3a3aaa5
josiah <josiahd.kitchin@gmail.com>
2025-02-13 23:56:47 -0800

    adding split block feature

--- tests/test_block.c
+++ tests/test_block.c
@@ -2,39 +2,52 @@
 #include "block.h"
 #include <criterion.h> 
 
-Test(block, get_free_block_found) { 
+Test(block, first_fit_found) { 
     // Test that a block of appropiate size is found 
     Block largerBlock; 
     largerBlock.size = 20; 
     largerBlock.next = NULL; 
-    largerBlock.free = true; 
 
     Block smallerBlock; 
     smallerBlock.size = 10; 
     smallerBlock.next = &largerBlock; 
-    smallerBlock.free = true; 
 
     Block* free_list = &smallerBlock; 
 
-    Block* outputBlock = get_free_block(&free_list, 15);
+    Block* outputBlock = first_fit(&free_list, 10);
     cr_assert(outputBlock, "Expected block to not be null");
-    cr_assert(outputBlock->size == 20, "Expected larger block to be chosen");
+    cr_assert(outputBlock->size == 10, "Expected larger block to be chosen");
 }
 
-Test(block, get_free_block_not_found) { 
+Test(block, first_fit_not_found) { 
     // Test if null is returned if no block is found 
     Block largerBlock; 
     largerBlock.size = 20; 
     largerBlock.next = NULL; 
-    largerBlock.free = true; 
 
     Block smallerBlock; 
     smallerBlock.size = 10; 
     smallerBlock.next = &largerBlock; 
-    smallerBlock.free = true; 
 
     Block* free_list = &smallerBlock; 
 
-    Block* outputBlock = get_free_block(&free_list, 30);
+    Block* outputBlock = first_fit(&free_list, 30);
     cr_assert(!outputBlock, "Expected block to be null");
 }
+
+Test(block, split_block) { 
+    // Test if null is returned if no block is found 
+    Block largerBlock; 
+    largerBlock.size = 20; 
+    largerBlock.next = NULL; 
+
+    Block smallerBlock; 
+    smallerBlock.size = 10; 
+    smallerBlock.next = &largerBlock; 
+
+    Block* free_list = &smallerBlock; 
+    Block* outputBlock = first_fit(&free_list, 15 + sizeof(Block));
+
+    cr_assert(free_list->size == 5, "Expected first_fit to split block, got %lu", free_list->size);
+}
+
